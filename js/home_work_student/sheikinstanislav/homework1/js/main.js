7f514abf647747d229ae851c56878fada0ff103e
sheikinstanislav <sheikinstanislav@gmail.com>
2018-11-06 17:09:56 +0300

    homework_js_1

---
+++ js/home_work_student/sheikinstanislav/homework1/js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var user;
+var dev;
+
+    dev = "stas";
+    user = dev;
+
+    dev = "ne stas";
+
+    console.log(dev);
+
+
+    var name = prompt("What is your name?");
+
+    confirm("Your name " +name +" ?")

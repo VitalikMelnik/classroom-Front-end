469b6f0a6d313a71bcfd77d85fa6643c95af1ece
Volodya Borshch <borshch2001@gmail.com>
2018-11-10 20:03:49 +0200

    lesson2

---
+++ js/home_work_student/Volodya Borshch/home-work2/Task2.js
@@ -0,0 +1,14 @@
+var number = prompt('Введіть будь-ласка число');
+if (number>0) {
+alert(1);
+}
+else if(number<0){
+
+	alert(-1);
+}
+else if (number==0) {
+	alert(0);
+}
+else{
+	alert('Ви не ввели число.По пробуйте ще раз!')
+}

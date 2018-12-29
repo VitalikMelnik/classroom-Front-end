56f999726a85d5da79259407fded7962060d571f
Vitalik Melnik <vitalikmelnik20000@gmail.com>
2018-12-29 21:56:29 +0200

    melnik vitalik js lesson 1

--- js/home_work_student/melnik_vitaliy/lesson 1/main.js
+++ js/home_work_student/melnik_vitaliy/lesson 1/main.js
@@ -1,7 +1,24 @@
-switch(browser) { 
-    case 'IE':
-    alert('О, да у вас IE!');  break;
-    case 'Chrome': case 'Firefox': case 'Safari': case:
+
+//Task 1
+
+var user,developer;
+
+developer = "Vitalik";
+user = developer;
+developer = 2;
+
+//console.log(user,developer);
 
 
-}
+// Task 2
+
+var name = prompt("What your name?", 0);
+var agree = confirm( name + ", your name ?");
+
+//console.log((agree)? name : null);
+
+
+
+
+
+
